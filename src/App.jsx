3ce3ac90aaa908ebf3fad94aae97afc6067cfdb0
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-25 18:46:50 -0500

    App css/jsx update Static boxes done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ function App() {
   return (
     <>
         <NavBar/>
-        <div>
+        <div className='px-20 grid grid-cols-1 md:grid-cols-2 gap-10 mt-10'>
+            <Job/>
+            <Job/>
+            <Job/>
+            <Job/>
             <Job/>
         </div>
     </>
