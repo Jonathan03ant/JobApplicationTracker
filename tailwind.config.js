6b78badb66fcdb7355ded062465bc615d5b937c2
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-25 18:47:45 -0500

    Tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
     theme: {
         extend: {
+            spacing: {
+                '100': '18rem',  // 400px
+                '120': '30rem',  // 480px
+                '140': '35rem',  // 560px
+                '160': '40rem',  // 640px
+              },
           fontFamily: {
             'crimson': ['Crimson Text', 'serif']
           }
