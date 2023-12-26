e92ad0b560390bb8d130e5e7b4bb3d7cca6228a9
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-26 18:48:05 -0500

    NavBar update with link, pathTo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
     render() {
         return (
             <div className='flex justify-end mt-10' >
                 <div className="navbar flex justify-between w-1/2 mr-20 px-2 text-lg text-black font-bold font-crimson bg-transparent">
-                    <button className=' bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> DashBoard </button>
-                    <button className=' bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> Add Application </button>
-                    <button className=' bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> Watchlist </button>
-                    <button className=' bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> Logout </button>
+                    <Link to="/dashboard" className=' bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> DashBoard </Link>
+                    <Link to="/add-application" className=' bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> Add Application </Link>
+                    <Link to="/watchlist" className=' bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> Watchlist </Link>
+                    <button className='bg-yellow-100 py-1.5 px-3.5 rounded-lg hover:bg-yellow-300'> Logout </button>
                 </div>
             </div>
         );
