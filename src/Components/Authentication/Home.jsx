e2e3aaf2c8cf15bf39d4aa690720d7cb366b533c
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-29 16:44:41 -0500

    Authentication setup initial

---
+++ src/Components/Authentication/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+            <div>
+                <button> signin</button>
+                <button> signup </button>
+                
+            </div>
+        );
+}
+
+
