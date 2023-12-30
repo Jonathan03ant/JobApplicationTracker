eb93e0a395ba2c5f58a0b1d3a7c025f201f7fa74
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-29 19:25:16 -0500

    Authentication done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+import { auth } from './Firebase'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
 import './App.css'
+
 import Home from './Components/Authentication/Home';
+import Login from './Components/Authentication/Login';
+import Signup from './Components/Authentication/Signup';
 
 import NavBar from './Components/NavBar'
 import Watchlist from './Components/Routers/Watchlist';
@@ -9,20 +15,52 @@ import AddApplication from './Components/Routers/AddApplication';
 import Dashboard from './Components/Routers/Dashboard';
 
 function App() {
-  return (
-    <Router>
-        <NavBar/>
-        <Routes>
 
-            <Route path="/home" element={<Home/>} />  
-            <ProtectedRoute path="/dashboard" element={<Dashboard/>} />  
-            <ProtectedRoute path="/add-application" element={<AddApplication/>} /> 
-            <ProtectedRoute path="/watchlist" element={<Watchlist/>} /> 
-            
-        </Routes>
-    </Router>
 
-  )
+    const [authUser, setAuthUser] = useState(null);
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser(user);
+            }else {
+                setAuthUser(null);
+            }
+        });
+
+        return () => {
+            unsub();
+        }
+    }, []);
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                setAuthUser(null);
+            });
+    };
+
+    return (
+        <Router>
+            {authUser && <NavBar handleLogout={handleLogout}/>}
+            <Routes>
+                {authUser ? (
+                    <>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/add-application" element={<AddApplication />} />
+                        <Route path="/watchlist" element={<Watchlist />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </>
+                )}
+            </Routes>
+        </Router>
+    );
+
 }
 
-export default App
+export default App;
