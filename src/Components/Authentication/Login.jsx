18112d15380a6c2c6d06369a63e34cc3d851f4f2
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-31 21:44:22 -0500

    Sign up and Sign in Completed

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {auth} from '../../Firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import HomeNav from './HomeNav';
+import GoogleIcon from '../Icons/GoogleSignin';
 
 export default function login() {
     const navigate = useNavigate();
@@ -26,30 +27,63 @@ export default function login() {
             })
     }
 
+    const signInWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then ((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+
+                const user = result.user;
+                navigate('/dashboard');
+                console.log(user)
+            })
+            .catch ((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log(errorCode, errorMessage, email, credential)
+            })
+    }
+
 
     return (
-        <div className='login flex justify-center space-x-5 mt-20'>
-            <form 
-                className='flex flex-col space-y-2'
-                onSubmit={signIn}  
-            >
-                    <h1> Login in </h1>
-                    <input 
-                        type='text' 
-                        placeholder='Email or Phone Number'
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+        <>
+            <HomeNav/>
+            <div className='container h-screen flex justify-center items-center space-x-40'>
 
-                    <input 
-                        type='password' 
-                        placeholder='Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button type='submit'> Sign In </button>
-            </form>
-        </div>
+                <form 
+                    className='flex flex-col space-y-2'
+                    onSubmit={signIn}  
+                >
+                        <h1 className='font-cara text-3xl text-blue-950'> <strong> LOGIN </strong></h1>
+                        <label className='font-cara text-xl'> Email</label>
+                        <input 
+                            className='w-72 p-2 h-10'
+                            type='text' 
+                            placeholder='Email or Phone Number'
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+
+                        <label className='font-cara text-xl'> Password</label>
+                        <input  
+                            className='w-72 h-10 p-2'
+                            type='password' 
+                            placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button className='bg-blue-200 w-54 hover:bg-gray-200 text-black py-2 px-4 rounded ' type='submit'> Sign In </button>
+                        <GoogleIcon onClick={signInWithGoogle} />
+
+                       
+                        
+                </form>
+           
+            </div>
+        </>
     );
 }
 
--- src/Components/Authentication/Signup.jsx
+++ src/Components/Authentication/Signup.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {auth} from '../../Firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import HomeNav from './HomeNav';
+import GoogleIcon from '../Icons/GoogleSignin';
 
 export default function Signup() {
     const navigate = useNavigate();
@@ -25,28 +27,61 @@ export default function Signup() {
             })
     }
 
-    return (
-        <div className='login flex justify-center space-x-5 mt-20'>
-            <form 
-                className='flex flex-col space-y-2'
-                onSubmit={signUp}  
-            >
-                    <h1> Create an Account </h1>
-                    <input 
-                        type='text' 
-                        placeholder='Email or Phone Number'
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+    const signUPWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then ((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
 
-                    <input 
-                        type='password' 
-                        placeholder='Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button type='submit'> Sign Up </button>
-            </form>
-        </div>
+                const user = result.user;
+                navigate('/dashboard');
+                console.log(user)
+            })
+            .catch ((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log(errorCode, errorMessage, email, credential)
+            })
+    }
+    return (
+        <>
+            <HomeNav/>
+            <div className='container h-screen flex justify-center items-center space-x-40'>
+
+                <form 
+                    className='flex flex-col space-y-2'
+                    onSubmit={signUp}  
+                >
+                        <h1 className='font-cara text-3xl text-blue-950'> <strong> SIGNUP </strong></h1>
+                        <label className='font-cara text-xl'> Email</label>
+                        <input 
+                            className='w-72 p-2 h-10'
+                            type='text' 
+                            placeholder='Email or Phone Number'
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+
+                        <label className='font-cara text-xl'> Password</label>
+                        <input  
+                            className='w-72 h-10 p-2'
+                            type='password' 
+                            placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button className='bg-blue-200 w-54 hover:bg-gray-200 text-black py-2 px-4 rounded ' type='submit'> Sign up</button>
+                        <GoogleIcon onClick={signUPWithGoogle} />
+
+
+                       
+                        
+                </form>
+           
+            </div>
+        </>
     );
 }
