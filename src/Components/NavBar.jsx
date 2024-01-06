8c3c7f5c8c079697e47e5e5da63e53f32bae6e04
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2024-01-05 19:55:54 -0500

    Nav Responsive update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -9,8 +9,8 @@ export default function NavBar({ handleLogout}) {
         navigate('/');
     }
     return (
-        <div className='flex justify-end mt-10' >
-            <div className="navbar flex justify-between w-1/2 mr-20 px-2 text-lg text-black font-bold font-cara bg-transparent">
+        <div className='flex justify-end mt-10 w-full'>
+            <div className="navbar flex justify-between w-1/2 mr-40 text-md text-black font-bold font-cara space-y-1">
                 <Link to="/dashboard" className=' bg-blue-200 py-1.5 px-3.5 rounded-sm  hover:bg-gray-200'> DashBoard </Link>
                 <Link to="/add-application" className=' bg-blue-200 py-1.5 px-3.5 rounded-sm  hover:bg-gray-200'> Add Application </Link>
                 <Link to="/watchlist" className='flex items-center bg-blue-200 py-1.5 px-3.5 rounded-sm  hover:bg-gray-200'> Watchlist <MdOutlineLabelImportant/> </Link>
