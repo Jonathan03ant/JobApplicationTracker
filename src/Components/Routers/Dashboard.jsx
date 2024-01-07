f2a5dcdca1476a2796caa2bccbbf0c94bf519012
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2024-01-07 16:48:14 -0500

    Dashboard showing welcome first Name now

--- src/Components/Routers/Dashboard.jsx
+++ src/Components/Routers/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { doc, getDoc, collection, getDocs } from 'firebase/firestore';
 import { auth, db } from '../../Firebase';
 import ApplicationTable from '../ApplicationTable';
 
@@ -7,10 +7,15 @@ import ApplicationTable from '../ApplicationTable';
 export default function DashBoard(){
     const [applications, setApplications] = useState([]);
     const [refresh, setRefresh] = useState(false);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         const fetchApplications = async () => {
             const userId = auth.currentUser.uid;
+            const userDoc = doc(db, 'users', userId);
+            const userSnapshot = await getDoc(userDoc);
+            setUser(userSnapshot.data());
+
             const applicationsRef = collection(db, 'users', userId, 'applications');
             const querySnapshot = await getDocs(applicationsRef);
             setApplications(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
@@ -25,6 +30,11 @@ export default function DashBoard(){
 
     return(
         <div className='px-10 py-36 w-full'>
+            {user && (
+               <div>
+                  <h1>Welcome, {user.firstName} </h1>
+               </div>
+           )}
             <ApplicationTable applications={applications} handleRefresh={handleRefresh}  />
         </div>
     )
